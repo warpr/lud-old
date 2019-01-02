8bf111755cf2eaeb0d6f71fd8dbf7d7af0c5e57f
kuno <kuno@frob.nl>
2019-01-02 14:30:48 -0500

    Update the files prettier runs on

--- tests/FileTest.php
+++ tests/FileTest.php
@@ -1,6 +1,13 @@
 <?php
+/**
+ *   This file is part of lûd, an opinionated browser based media player.
+ *   Copyright (C) 2018  Kuno Woudt <kuno@frob.nl>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of copyleft-next 0.3.1.  See copyleft-next-0.3.1.txt.
+ */
 
-require_once(dirname(__FILE__) . '/../lib/file.php');
+require_once __DIR__ . '/../lib/file.php';
 
 use PHPUnit\Framework\TestCase;
 
@@ -28,4 +35,3 @@ class FileTest extends TestCase
         $this->assertEquals("/foo/bar", abspath("/foo/bar"), "root anchored");
     }
 }
-
